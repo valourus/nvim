e4e9a8dae0b1bcb86d3481c84b57fdc1dba1a275
koenvanderels <koen@example.com>
2025-06-08 10:43:51 +0200

    fixed some bugs

--- lua/custom/plugins/treeview.lua
+++ lua/custom/plugins/treeview.lua
@@ -1,3 +1,35 @@
+_G.last_neotree_package_json_execution_time = 0
+
+function _G.openNeotreeAndPackageJsonConditionalSplit()
+  if vim.fn.isdirectory(vim.fn.getcwd() .. "/.git") then
+    local current_time = vim.fn.reltime()[1]
+
+    -- Step 1: Check if more than 1 second has passed since the last execution (debounce)
+    if current_time - _G.last_neotree_package_json_execution_time >= 1 then
+      -- Step 2: If debounced, update the last execution time immediately.
+      _G.last_neotree_package_json_execution_time = current_time
+
+      -- Step 3: Schedule the actual commands to run after a 200ms delay
+      vim.defer_fn(function()
+        -- Always reveal Neotree
+        vim.cmd("Neotree reveal reveal_force_cwd")
+
+        -- Get the number of currently open windows
+        local num_open_windows = #vim.api.nvim_list_wins()
+
+        -- Only vsplit if there are exactly 2 windows open (the current one and one other)
+        if num_open_windows == 1 then
+          local current_window_width = vim.api.nvim_win_get_width(0)
+          local desired_width = math.floor(current_window_width * 0.80)
+
+          vim.cmd(desired_width .. "vsplit enew")
+        end
+      end, 200) -- 200 milliseconds delay
+    end
+  end
+end
+
+
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -8,7 +40,7 @@ return {
     },
     keys = {
         { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
-        { "<A-1>", "<cmd>Neotree reveal<cr>", desc = "Reveal current file in Neo-tree" },
+        { "<A-1>",     "<cmd>Neotree reveal<cr>", desc = "Reveal current file in Neo-tree" },
     },
     opts = {
         toggle = false,
@@ -58,16 +90,8 @@ return {
         vim.api.nvim_create_autocmd({ "DirChanged" }, {
             group = "NeoTreeProjectAutoOpen",
             callback = function()
-                if vim.fn.isdirectory(vim.fn.getcwd() .. "/.git") then
-                    vim.cmd("Neotree reveal reveal_force_cwd")
-                    vim.defer_fn(function()
-                        if vim.fn.filereadable("package.json") then
-                            local current_window_width = vim.api.nvim_win_get_width(0)
-                            local desired_width = math.floor(current_window_width * 0.80)
-                            vim.cmd(desired_width .. "vsplit package.json")
-                        end
-                    end, 50)
-                end
+              vim.cmd("Neotree reveal reveal_force_cwd")
+              _G.openNeotreeAndPackageJsonConditionalSplit();
             end,
         })
     end,
