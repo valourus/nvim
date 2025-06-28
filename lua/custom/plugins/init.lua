b5717e59b437b3754a1b27945ce45b4ed0c1ed1c
koenvanderels <koen.vanderels@pangaea.nl>
2025-06-28 12:08:48 +0200

    added lazygit and remove the help binding on f1

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,7 @@
 vim.opt.shadafile = 'NONE'
 vim.opt.relativenumber = true
 
+vim.keymap.set({'n', 'i', 'c', 's', 'o', 'x'}, '<F1>', '<Nop>', { noremap = true, silent = true });
 return {
   'nvim-tree/nvim-web-devicons',
   'pocco81/auto-save.nvim',
