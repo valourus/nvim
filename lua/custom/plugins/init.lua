43b08dde9abdfc904a8d6a46c0385f4733c56aba
koenvanderels <koen@example.com>
2025-06-30 00:03:45 +0200

    fixed commenting out code in tsx files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,8 +1,10 @@
 vim.opt.shadafile = 'NONE'
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
 
 vim.keymap.set({'n', 'i', 'c', 's', 'o', 'x'}, '<F1>', '<Nop>', { noremap = true, silent = true });
 return {
   'nvim-tree/nvim-web-devicons',
   'pocco81/auto-save.nvim',
+  'rcarriga/nvim-notify'
 }
--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -2,14 +2,10 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    -- Set the direction to "horizontal" to make it appear at the bottom
     direction = 'horizontal',
-    -- You can also specify the size (height)
-    size = 15, -- for example, 15 lines tall
-    -- Other options you might want to configure:
-    open_mapping = [[<C-\>]], -- Your preferred keybinding to toggle
-    shade_terminals = true, -- To darken the terminal background
-    start_in_insert = true, -- Start in insert mode when opening
-    -- ... and so on
+    size = 18,
+    open_mapping = [[<C-\>]],
+    shade_terminals = true,
+    start_in_insert = true,
   },
- }
+}
