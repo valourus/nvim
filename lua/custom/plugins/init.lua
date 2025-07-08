f9195d2f3aa349d1d2bec94e7564d00f5553aa1c
koenvanderels <koen.vanderels@pangaea.nl>
2025-07-08 17:34:20 +0200

    formatted startscreen

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,9 +2,10 @@ vim.opt.shadafile = 'NONE'
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 
+-- Remove the help buffer opening with <F1>
 vim.keymap.set({'n', 'i', 'c', 's', 'o', 'x'}, '<F1>', '<Nop>', { noremap = true, silent = true });
+
 return {
   'nvim-tree/nvim-web-devicons',
   'pocco81/auto-save.nvim',
-  'rcarriga/nvim-notify'
 }
--- lua/custom/plugins/start-screen.lua
+++ lua/custom/plugins/start-screen.lua
@@ -1,85 +1,87 @@
 return {
-    'git@github.com:goolord/alpha-nvim.git',
-    priority = 1000, -- Make sure alpha loads before other plugins
-    lazy = false,    -- Load alpha immediately when Neovim starts
-    config = function()
-        local alpha = require 'alpha'
-        local dashboard = require 'alpha.themes.dashboard'
+  'git@github.com:goolord/alpha-nvim.git',
+  priority = 1000, -- Make sure alpha loads before other plugins
+  lazy = false, -- Load alpha immediately when Neovim starts
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
 
-        -- Customize the dashboard theme
-        dashboard.section.header.val = {
-            '=====================================================================',
-            '=====================================================================',
-            '========                                    .-----.          ========',
-            '========         .----------------------.   | === |          ========',
-            '========         |.-""""""""""""""""""-.|   |-----|          ========',
-            '========         ||                    ||   | === |          ========',
-            '========         ||       NEOVIM       ||   |-----|          ========',
-            '========         ||                    ||   | === |          ========',
-            '========         ||                    ||   |-----|          ========',
-            '========         ||:q                  ||   |:::::|          ========',
-            '========         | -..................- |   |____o|          ========',
-            '========         `"")----------------(""`   ___________      ========',
-            '========        /::::::::::|  |::::::::::\\  \\ no mouse \\     ========',
-            '========       /:::========|  |==hjkl==:::\\  \\ required \\    ========',
-            '========      \'""""""""""""\'  \'""""""""""""\'  \'""""""""""\'   ========',
-            '========                                                     ========',
-            '=====================================================================',
-            '====================================================================='
-        }
-        dashboard.section.header.width = 300
-        dashboard.section.header.height = 400
-        dashboard.section.buttons.val = {
-            dashboard.button('f', '  New File', ':enew<CR>'),
-            dashboard.button('p', '  Find Project', ':Telescope neovim-project discover<CR>'),
-            dashboard.button('q', '  Quit', ':qa<CR>'),
-        }
+    -- Customize the dashboard theme
+    dashboard.section.header.val = {
+      '=====================================================================',
+      '=====================================================================',
+      '========                                    .-----.          ========',
+      '========         .----------------------.   | === |          ========',
+      '========         |.-""""""""""""""""""-.|   |-----|          ========',
+      '========         ||                    ||   | === |          ========',
+      '========         ||       NEOVIM       ||   |-----|          ========',
+      '========         ||                    ||   | === |          ========',
+      '========         ||                    ||   |-----|          ========',
+      '========         ||:q                  ||   |:::::|          ========',
+      '========         | -..................- |   |____o|          ========',
+      '========         `"")----------------(""`   ___________      ========',
+      '========        /::::::::::|  |::::::::::\\  \\ no mouse \\     ========',
+      '========       /:::========|  |==hjkl==:::\\  \\ required \\    ========',
+      '========      \'""""""""""""\'  \'""""""""""""\'  \'""""""""""\'   ========',
+      '========                                                     ========',
+      '=====================================================================',
+      '=====================================================================',
+    }
+    dashboard.section.header.width = 300
+    dashboard.section.header.height = 400
+    dashboard.section.buttons.val = {
+      dashboard.button('n', '  New File', ':enew<CR>'),
+      dashboard.button('f', ' Find File', function()
+        require('telescope.builtin').find_files { cwd = '/home/koenvanderels/', hidden = true }
+      end),
+      dashboard.button('p', '  Find Project', ':Telescope neovim-project discover<CR>'),
+      dashboard.button('q', '  Quit', ':qa<CR>'),
+    }
 
-        local function footer()
-            local datetime = os.date '%Y-%m-%d %H:%M'
-            local version = vim.version()
-            local nvim_version = string.format('     v%d.%d.%d', version.major, version.minor, version.patch)
-            return {
-                '',
-                datetime,
-                '',
-                nvim_version,
-                '',
-            }
+    local function footer()
+      local datetime = os.date '%Y-%m-%d %H:%M'
+      local version = vim.version()
+      local nvim_version = string.format('     v%d.%d.%d', version.major, version.minor, version.patch)
+      return {
+        '',
+        datetime,
+        '',
+        nvim_version,
+        '',
+      }
+    end
+    dashboard.section.footer.val = footer()
+    dashboard.section.footer.opts = {
+      position = 'center',
+    }
+    dashboard.section.footer.opts.hl = 'Constant'
+    -- Only need to call setup once
+    alpha.setup(dashboard.config)
+    vim.keymap.set('n', '<Leader>qq', function()
+      require('neo-tree.command').execute { action = 'close' }
+      require('alpha').start()
+    end, { noremap = true, silent = true })
+
+    -- Create a dedicated augroup for alpha
+    local alpha_group = vim.api.nvim_create_augroup('AlphaStartScreen', { clear = true })
+
+    vim.api.nvim_create_autocmd('VimEnter', {
+      group = alpha_group,
+      callback = function()
+        for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_option(bufnr, 'buflisted') then
+            vim.api.nvim_buf_delete(bufnr, { force = true })
+          end
         end
-        dashboard.section.footer.val = footer()
-        dashboard.section.footer.opts = {
-            position = 'center',
-        }
-        dashboard.section.footer.opts.hl = 'Constant'
-        -- Only need to call setup once
-        alpha.setup(dashboard.config)
-        vim.keymap.set('n', '<Leader>qq', function()
-            require('neo-tree.command').execute({ action = 'close' })
-            require('alpha').start()
-        end, { noremap = true, silent = true })
+        vim.defer_fn(function()
+          -- Start alpha
+          require('alpha').start()
 
-
-        -- Create a dedicated augroup for alpha
-        local alpha_group = vim.api.nvim_create_augroup("AlphaStartScreen", { clear = true })
-
-        vim.api.nvim_create_autocmd("VimEnter", {
-            group = alpha_group,
-            callback = function()
-                for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-                    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_option(bufnr, 'buflisted') then
-                        vim.api.nvim_buf_delete(bufnr, { force = true })
-                    end
-                end
-                vim.defer_fn(function()
-                    -- Start alpha
-                    require('alpha').start()
-
-                    -- Hide command line
-                    vim.cmd('set cmdheight=0')
-                end, 10)
-            end,
-            desc = "Start Alpha when Neovim is opened with no arguments"
-        })
-    end,
+          -- Hide command line
+          vim.cmd 'set cmdheight=0'
+        end, 10)
+      end,
+      desc = 'Start Alpha when Neovim is opened with no arguments',
+    })
+  end,
 }
