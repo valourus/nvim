8f4e3c1a111a213ff33b67466dc017e83cda913c
koenvanderels <koen.vanderels@pangaea.nl>
2025-08-04 08:30:35 +0200

    only load merge tool when needed

--- lua/custom/plugins/merge-tool.lua
+++ lua/custom/plugins/merge-tool.lua
@@ -1,3 +1,15 @@
 return {
-  -- "sindrets/diffview.nvim"
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>ld", "<cmd>DiffviewOpen<CR>", desc = "Open Diffview" },
+  }, -- Loads only when these keybindings are pressed
+  config = function()
+    require("diffview").setup({
+      -- view = {
+      --   default = {
+      --     layout = "diff2_fill",
+      --   },
+      -- },
+    })
+  end,
 }
