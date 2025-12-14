b4ac2d00b7e8241916ac416342ffaecc7dcd5ed4
koenvanderels <koenvanderels@pop-os.home>
2025-12-14 15:02:31 +0100

    added a function to skip warning while using ][d

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,14 +6,33 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.cursorline = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = 'unnamedplus'
 
-vim.keymap.set("i", "<C-v>", function()
-  return vim.fn.getreg("+")
+vim.keymap.set('i', '<C-v>', function()
+  return vim.fn.getreg '+'
 end, { expr = true, noremap = true })
-vim.keymap.set("n", "<C-v>", '"+p')
+vim.keymap.set('n', '<C-v>', '"+p')
 
-vim.keymap.set({'n', 'i', 'c', 's', 'o', 'x'}, '<F1>', '<Nop>', { noremap = true, silent = true });
+-- disable the F1 help key, I kept hitting it accidentally
+vim.keymap.set({ 'n', 'i', 'c', 's', 'o', 'x' }, '<F1>', '<Nop>', { noremap = true, silent = true })
+
+local ERROR_SEVERITY = vim.diagnostic.severity.ERROR
+
+-- Overwrite ]d to jump to the NEXT Error only
+vim.keymap.set('n', ']d', function()
+  vim.diagnostic.jump {
+    count = 1,
+    severity = ERROR_SEVERITY,
+  }
+end, { desc = 'Next ERROR Diagnostic' })
+
+-- Overwrite [d to jump to the PREVIOUS Error only
+vim.keymap.set('n', '[d', function()
+  vim.diagnostic.jump {
+    count = -1,
+    severity = ERROR_SEVERITY,
+  }
+end, { desc = 'Previous ERROR Diagnostic' })
 
 return {
   'nvim-tree/nvim-web-devicons',
