08615ef5c5e1a097481ec1b827f9f23c52097a98
koenvanderels <koenvanderels@pop-os.home>
2025-12-28 11:46:07 +0100

    removed the grr binding because it caused nvim to wait for me to complete the while gr did the same thing

--- init.lua
+++ init.lua
@@ -592,6 +592,9 @@ require('lazy').setup({
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+          -- Remove default grr mapping (uses quickfix instead of Telescope)
+          vim.keymap.del('n', 'grr')
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
